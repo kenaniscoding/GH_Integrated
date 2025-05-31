77806b0f3b7911e67669cc5b8d1669c54a902569
kenaniscoding <banalkenan@gmail.com>
2025-05-31 18:57:11 +0800

    fix the admin interface

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@ include('session.php');
 $servername = "localhost";
 $username = "root";
 $password = "onelasalle"; // Set your password if needed
-$dbname = "db2";
+$dbname = "db3";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
